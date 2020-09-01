ff3995d4bb1e8a698ad187017959ca0f5b1c147f
Firas <>
2020-09-01 11:40:05 +0200

    Add Lb03 deployment template and script

---
+++ labs/lab03/deployment-script-lab03.ps1
@@ -0,0 +1,27 @@
+# Parameters
+$newDeploymentName="AZ204Lab03Deployment"
+$resourceGroupName="StorageMedia"
+$location="East US"
+$storageAccountNameSuffix="az204lab03" # to be updated (yourname)
+
+$ParametersObj = @{
+    newDeploymentName = $newDeploymentName
+    resourceGroupName = $resourceGroupName
+    location = $location
+    storageAccountNameSuffix = $storageAccountNameSuffix
+}
+
+$storageAccountName="mediastor"+$storageAccountNameSuffix
+
+# Deployment
+New-AzDeployment -Location $location -TemplateFile 'template-lab03.json' -TemplateParameterObject $ParametersObj
+
+# Retrieve Storage Account key
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $storageAccountName) | Where-Object {$_.KeyName -eq "key1"}
+
+# Get Storage Account Context
+$context = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey.value
+
+# Upload blobs
+Set-AzStorageBlobContent -Context $context -Container "raster-graphics" -File "images/graph.jpg" -Blob "graph.jpg"
+Set-AzStorageBlobContent -Context $context -Container "vector-graphics" -File "images/graph.svg" -Blob "graph.svg"
